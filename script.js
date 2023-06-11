e54a04b5a434269dbbfc711b60521d6727d3c6b4
Filip J <127353735+filipjacko@users.noreply.github.com>
2023-06-11 09:35:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.addEventListener('scroll', function() {
   var logo = document.querySelector('.logo');
 
-  // Calculate the rotation angle based on the scroll position
-  var rotation = window.scrollY / 5; // Adjust the division factor to control the rotation speed
+
+  var rotation = window.scrollY / 5; 
 
   // Apply the rotation transform to the logo
   logo.style.transform = 'rotate(' + rotation + 'deg)';
